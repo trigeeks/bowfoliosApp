3423b89d2dd12ad821fed4771b50fc98fd3fbc2b
tianhuizhou <tianhui@hawaii.edu>
2020-09-07 10:02:14 -1000

    finished
    The functionalities of Sign In/Up are completed.
    have the basic UI.

---
+++ Bowfolios-trigeeks/Bowfolios-trigeeks/Model/SessionStore.swift
@@ -0,0 +1,68 @@
+//
+//  SessionStore.swift
+//  Bowfolios-trigeeks
+//
+//  Created by Tianhui Zhou on 9/7/20.
+//  Copyright © 2020 trigeeks. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import Combine
+import FirebaseAuth
+
+class SessionStore: ObservableObject {
+    var didChange = PassthroughSubject<SessionStore, Never>()
+    @Published var session: User? {didSet {self.didChange.send(self) }}
+    
+    var handle: AuthStateDidChangeListenerHandle?
+    
+    func listen() {
+        handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
+            if let user = user {
+                self.session = User(uid: user.uid, email: user.email!)
+                print(self.session?.uid ?? "test for id")
+            } else {
+                self.session = nil
+            }
+        })
+    }
+    
+    func signUp(email: String, password: String, handler: @escaping AuthDataResultCallback) {
+        Auth.auth().createUser(withEmail: email, password: password, completion: handler)
+    }
+    
+    func signIn(email: String, password: String, handler: @escaping AuthDataResultCallback) {
+        Auth.auth().signIn(withEmail: email, password: password, completion: handler)
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            self.session = nil
+            print("Sign out successfully")
+        } catch {
+            print("Error sign out!")
+        }
+    }
+    
+    func unbind() {
+        if let handle = handle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
+    
+    deinit {
+        unbind()
+    }
+}
+
+struct User {
+    var uid: String
+    var email: String
+    
+    init(uid: String, email:String) {
+        self.uid = uid
+        self.email = email
+    }
+}
---
+++ Bowfolios-trigeeks/Bowfolios-trigeeks/Views/AuthView.swift
@@ -0,0 +1,163 @@
+//
+//  AuthView.swift
+//  Bowfolios-trigeeks
+//
+//  Created by Tianhui Zhou on 9/7/20.
+//  Copyright © 2020 trigeeks. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var error: String = ""
+    @EnvironmentObject var session: SessionStore
+    
+    func signIn() {
+        print("here")
+        session.signIn(email: email, password: password) { (result, error) in
+            if let error = error {
+                self.error = error.localizedDescription
+            } else {
+                self.email = ""
+                self.password = ""
+            }
+        }
+        
+    }
+    
+    var body: some View {
+        VStack{
+            Text("Welcome back!")
+                .font(.system(size: 32, weight: .heavy))
+            
+            Text("Sign in to continue")
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(.gray)
+            
+            VStack{
+                TextField("Email address", text: $email)
+                    .font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color(.black), lineWidth: 1))
+                    
+                
+                SecureField("Password", text: $password).font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color(.black), lineWidth: 1))
+                    
+            }.padding(.vertical, 70)
+            
+            Button(action: signIn){
+                Text("Sign In")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+            }
+            
+            if (error != "") {
+                Text(error)
+                    .font(.system(size: 14, weight: .semibold))
+                    .foregroundColor(.red)
+                    .padding()
+                
+                
+            }
+            
+            Spacer()
+            
+            NavigationLink (destination: SignUpView()){
+                HStack{
+                    Text("Don't have an account?")
+                        .font(.system(size: 14, weight:.light))
+                        .foregroundColor(.primary)
+                    Text("Create an account")
+                        .font(.system(size: 14,weight: .bold))
+                }
+                
+            }
+            
+        }.padding(.horizontal, 32)
+        
+    }
+}
+
+struct SignUpView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var error: String = ""
+    @EnvironmentObject var session: SessionStore
+    
+    func signUp(){
+        session.signUp(email: email, password: password) { (result, error) in
+            if let error = error {
+                self.error = error.localizedDescription
+            } else {
+                self.email = ""
+                self.password = ""
+            }
+        }
+    }
+    var body: some View {
+        VStack{
+            Text("Create Account")
+                .font(.system(size: 32, weight: .heavy))
+            
+            Text("Sign Up to get started")
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(.gray)
+            
+            VStack{
+                TextField("Email address", text: $email)
+                    .font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color(.black), lineWidth: 1))
+                    
+                
+                SecureField("Password", text: $password).font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color(.black), lineWidth: 1))
+                    
+            }.padding(.vertical, 70)
+            
+            Button(action: signUp){
+                Text("Sign Up")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+            }
+            
+            
+            if (error != "") {
+                Text(error)
+                    .font(.system(size: 14, weight: .semibold))
+                    .foregroundColor(.red)
+                    .padding()
+                
+                
+            }
+            
+            Spacer()
+            
+            
+        }.padding(.horizontal, 32)
+    }
+}
+
+struct AuthView: View {
+    var body: some View {
+        NavigationView{
+            SignInView()
+        }
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+    }
+}
