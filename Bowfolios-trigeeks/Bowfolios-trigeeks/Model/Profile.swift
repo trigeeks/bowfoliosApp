95e52d3f22550a186e935e3f4835784737c0397b
tianhuizhou <tianhui@hawaii.edu>
2020-09-08 01:02:02 -1000

    Initialize

---
+++ Bowfolios-trigeeks/Bowfolios-trigeeks/Model/Profile.swift
@@ -0,0 +1,40 @@
+//
+//  Profile.swift
+//  Bowfolios-trigeeks
+//
+//  Created by Tianhui Zhou on 9/8/20.
+//  Copyright © 2020 trigeeks. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Profile: Codable, Identifiable {
+    
+    @DocumentID var id: String?
+    var firstName: String
+    var lastName: String
+    var bio: String
+    var email: String
+    var title: String
+    var projects: [String]
+    var interests: [String]
+    var picture: String
+    
+    
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case id
+        case firstName
+        case lastName
+        case bio
+        case email
+        case title
+        case projects
+        case interests
+        case picture
+        
+    }
+}
