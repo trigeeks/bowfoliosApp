24d9c4ddb14f4c2cc2394984d6217a7abf8a54e5
Yuhan Jiang <jiangyuh@hawaii.edu>
2020-09-08 18:33:14 -1000

    ui done

--- Bowfolios-trigeeks/Bowfolios-trigeeks/Views/SubView/ProfileRowView.swift
+++ Bowfolios-trigeeks/Bowfolios-trigeeks/Views/SubView/ProfileRowView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct ProfileRowView: View {
     @State var profile: Profile
+    @ObservedObject var projects = ProjectViewModel()
+    
     var body: some View {
         VStack(alignment: .leading) {
             VStack(alignment: .leading) {
@@ -32,8 +34,44 @@ struct ProfileRowView: View {
                         .frame(width: 60, height:60)
                 }
                 Text(self.profile.bio)
+                    .fixedSize(horizontal: false, vertical: true)
+                
+             
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 10) {
+                        ForEach(profile.interests, id: \.self) {interest in
+                            Text("  \(interest)  ")
+                                .fontWeight(.semibold)
+                                .background(LinearGradient(gradient: Gradient(colors: [Color("bg2"), Color("bg1")]), startPoint: .leading, endPoint: .trailing))
+                                .foregroundColor(Color.white)
+                                .cornerRadius(5)
+                            
+                            
+                        }
+                    }
+                }
+                
+                Divider()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Projects").fontWeight(.semibold)
+                        Spacer()
+                    }
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            ForEach(profile.projects, id: \.self) { project in
+                                
+                                Image("turtlerock")
+                                    .resizable().frame(width: 50, height: 50).cornerRadius(50)
+                            }
+                        }
+                    }
+                }
                 
             }
+        }.onAppear() {
+            self.projects.fetchData()
         }
     }
 }
