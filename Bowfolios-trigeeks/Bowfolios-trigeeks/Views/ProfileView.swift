818331e5921607064863d3a6bae1b6deaaf1b055
Yuhan Jiang <jiangyuh@hawaii.edu>
2020-09-08 14:06:27 -1000

    step1

---
+++ Bowfolios-trigeeks/Bowfolios-trigeeks/Views/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  Bowfolios-trigeeks
+//
+//  Created by Yuhan Jiang on 2020/9/8.
+//  Copyright © 2020 trigeeks. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
