18a4a9f0e8d59b1726ecc431db38f154a75538b6
Yuhan Jiang <jiangyuh@hawaii.edu>
2020-09-26 10:58:40 -1000

    color

--- Bowfolios-trigeeks/Bowfolios-trigeeks/Views/AuthView.swift
+++ Bowfolios-trigeeks/Bowfolios-trigeeks/Views/AuthView.swift
@@ -175,12 +175,11 @@ struct SignUpView: View {
         ZStack {
             VStack {
                 HStack {
-                    Spacer()
-                    VStack(alignment: .trailing) {
+                    VStack() {
                     Text("Create Account")
                         .fontWeight(.heavy)
                         .font(.largeTitle)
-                        .foregroundColor(Color(#colorLiteral(red: 0.2013712525, green: 0.433947742, blue: 0.8717361093, alpha: 1)))
+                        .foregroundColor(Color("bg2"))
                     
                     Text("Sign Up to get started")
                         .font(.system(size: 18, weight: .medium))
@@ -195,7 +194,7 @@ struct SignUpView: View {
                         Text("Email")
                             .fontWeight(.semibold)
                             .font(.title)
-                            .foregroundColor(Color(#colorLiteral(red: 0.2013712525, green: 0.433947742, blue: 0.8717361093, alpha: 1)))
+                            .foregroundColor(Color("bg2"))
                         TextField("Email address", text: $email)
                             .modifier(TextFieldModifier())
                     }
@@ -208,7 +207,7 @@ struct SignUpView: View {
                         Text("Password")
                             .fontWeight(.semibold)
                             .font(.title)
-                            .foregroundColor(Color(#colorLiteral(red: 0.2013712525, green: 0.433947742, blue: 0.8717361093, alpha: 1)))
+                            .foregroundColor(Color("bg2"))
                         HStack {
                             
                             if isVisiable {
@@ -242,10 +241,10 @@ struct SignUpView: View {
                 Button(action: signUp){
                     Text("Sign Up").fontWeight(.semibold)
                         .font(.title2)
-                        .foregroundColor(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+                        .foregroundColor(Color("bg5"))
                 }.padding().buttonStyle(LongButtonStyle())
                 }.background(
-                    RoundedRectangle(cornerRadius: 15).foregroundColor(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+                    RoundedRectangle(cornerRadius: 15).foregroundColor(Color("bg5"))
                         .shadow(color: Color.black.opacity(0.2), radius: 10, x: 8, y: 10)
                         .shadow(color: Color.white.opacity(0.7), radius: 10, x: -10, y: -10)
                 )
@@ -255,7 +254,7 @@ struct SignUpView: View {
                 
             }.padding(.horizontal, 32)
                 .padding(.vertical, 50)
-            .background(Color(#colorLiteral(red: 0.8864660859, green: 0.8863860965, blue: 0.9189570546, alpha: 1)))
+            .background(Color("bg5"))
             .edgesIgnoringSafeArea(.all)
             
             if showAlert {
