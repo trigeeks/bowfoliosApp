607ded445fdcfb6fb402872489dc758509ca8dc2
tianhuizhou <tianhui@hawaii.edu>
2020-09-29 15:42:44 -1000

    improve the quality of image
    use a better quality of image. better resolution

--- Bowfolios-trigeeks/Bowfolios-trigeeks/Views/ContentView.swift
+++ Bowfolios-trigeeks/Bowfolios-trigeeks/Views/ContentView.swift
@@ -50,7 +50,7 @@ struct ContentView: View {
             } else {
                 ZStack{
                     Color("bg6").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                    Image("icon").rotation3DEffect(
+                    Image("logo-1").rotation3DEffect(
                         .degrees(angle),
                         axis: (x: 0.0, y: 1.0, z: 0.0)
                     ).opacity(opacity).scaleEffect(scale)
